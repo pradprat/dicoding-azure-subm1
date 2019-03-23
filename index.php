dec2ea39a0ad3c3aa4fb3d00d59e36dc75adf71f
pradika pratama <pradikafp14@gmail.com>
2019-03-23 11:04:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,19 +10,33 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     </head>
 <body>
-    <h1>hello</h1>
-    <div class="input-field col s6">
-        <input id="nama" type="text" class="validate">
-        <label class="active" for="nama">Nama</label>
+    <div class="container">
+        <h1>hello</h1>
+
+        <div class="input-field col s6">
+            <input id="nama" type="text" class="validate">
+            <label class="active" for="nama">Nama</label>
+        </div>
+        <div class="input-field col s6">
+            <input id="nim" type="text" class="validate">
+            <label class="active" for="nim">NIM</label>
+        </div>
+        <div class="input-field col s6">
+            <input id="hp" type="text" class="validate">
+            <label class="active" for="hp">No.HP</label>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+        </button>
+        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+        </button>
+            <a class="waves-effect waves-light btn-small">
+            <i class="material-icons right">cloud</i>display data
+        </a>
     </div>
-    <div class="input-field col s6">
-        <input id="nim" type="text" class="validate">
-        <label class="active" for="nim">NIM</label>
-    </div>
-    <div class="input-field col s6">
-        <input id="hp" type="text" class="validate">
-        <label class="active" for="hp">No.HP</label>
-    </div>
+    
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="main.js"></script>
